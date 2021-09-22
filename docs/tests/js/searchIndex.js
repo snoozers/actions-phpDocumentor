1bf17a349b2aa57f43a5cfc371a47639547b13b5
snoozers <snoozers@users.noreply.github.com>
2021-09-22 11:00:15 +0000

    docs: テスト仕様書生成

--- docs/tests/js/searchIndex.js
+++ docs/tests/js/searchIndex.js
@@ -1,16 +1,6 @@
 Search.appendIndex(
     [
                 {
-            "fqsen": "\\Tests\\Feature\\ExampleTest",
-            "name": "ExampleTest",
-            "summary": "",
-            "url": "classes/Tests-Feature-ExampleTest.html"
-        },                {
-            "fqsen": "\\Tests\\Feature\\ExampleTest\u003A\u003AtestBasicTest\u0028\u0029",
-            "name": "testBasicTest",
-            "summary": "A\u0020basic\u0020test\u0020example.",
-            "url": "classes/Tests-Feature-ExampleTest.html#method_testBasicTest"
-        },                {
             "fqsen": "\\Tests\\TestCase",
             "name": "TestCase",
             "summary": "",
@@ -26,10 +16,10 @@ Search.appendIndex(
             "summary": "A\u0020basic\u0020test\u0020example.",
             "url": "classes/Tests-Unit-ExampleTest.html#method_testBasicTest1"
         },                {
-            "fqsen": "\\Tests\\Unit\\ExampleTest\u003A\u003A\u30C6\u30B9\u30C8\u30E1\u30BD\u30C3\u30C91desu\u0028\u0029",
-            "name": "\u30C6\u30B9\u30C8\u30E1\u30BD\u30C3\u30C91desu",
+            "fqsen": "\\Tests\\Unit\\ExampleTest\u003A\u003A\u30C6\u30B9\u30C8\u30E1\u30BD\u30C3\u30C91desudd\u0028\u0029",
+            "name": "\u30C6\u30B9\u30C8\u30E1\u30BD\u30C3\u30C91desudd",
             "summary": "\u6982\u8981",
-            "url": "classes/Tests-Unit-ExampleTest.html#method_%E3%83%86%E3%82%B9%E3%83%88%E3%83%A1%E3%82%BD%E3%83%83%E3%83%891desu"
+            "url": "classes/Tests-Unit-ExampleTest.html#method_%E3%83%86%E3%82%B9%E3%83%88%E3%83%A1%E3%82%BD%E3%83%83%E3%83%891desudd"
         },                {
             "fqsen": "\\Tests\\Unit\\ExampleTest\u003A\u003A\u30C6\u30B9\u30C8\u30E1\u30BD\u30C3\u30C92\u0028\u0029",
             "name": "\u30C6\u30B9\u30C8\u30E1\u30BD\u30C3\u30C92",
@@ -40,11 +30,6 @@ Search.appendIndex(
             "name": "\\",
             "summary": "",
             "url": "namespaces/default.html"
-        },                {
-            "fqsen": "\\Tests\\Feature",
-            "name": "Feature",
-            "summary": "",
-            "url": "namespaces/tests-feature.html"
         },                {
             "fqsen": "\\Tests",
             "name": "Tests",
